c744fe430ed6c4230ccb094fe14fd2ca64cfaa04
JonasWflr <60130971+JonasWflr@users.noreply.github.com>
2021-02-17 23:17:04 +0100

    major changes from Jonathan (banner, text, no sidebar)

--- FromDawnTilDusk.py
+++ FromDawnTilDusk.py
@@ -53,7 +53,7 @@ def fill_with_dummy_data(df):
     return(df)
 
 # READ FROM GOOGLE
-st.cache(ttl=60*10)
+st.cache(ttl=60*1)
 def read_data():
     GOOGLE_SPREADSHEET = 'https://docs.google.com/spreadsheets/d/1mV3lzutv90A3xjoTANt1-qg7XxZ-8-7LkgdfldQr8CA/gviz/tq?tqx=out:csv'
     response = requests.get(GOOGLE_SPREADSHEET)
@@ -63,60 +63,36 @@ def read_data():
     df_data_google = pd.read_csv(TEST, sep=',')
     return(df_data_google)
 
-#################
-# CHOOSE DATE
-today = datetime.date.today()
-# for testing:
-#today = datetime.date(2021,3,7)
-#st.write(today)
-date_race = datetime.date(2021,3,6)
-remaining_days = (date_race -today).days
-df_data_google = read_data()
-if remaining_days>0:
-    date = st.sidebar.radio('Wähle Datum',('heute','postrace'),0)
-    if(date=='postrace'):
-        today_selected = datetime.date(2021, 3, 7)
-        df_selected = fill_with_dummy_data(df_data_google)#df_data_dummy
-        st.header('Die Plots zeigen Dummydaten')
-    else:
-        today_selected = datetime.date.today()
-        df_selected = df_data_google
-else:
-    date = st.sidebar.radio('Wähle Datum', ('prerace','heute'), 1)
-    if (date == 'prerace'):
-        today_selected = datetime.date(2021, 3, 5)
-        df_selected = df_data_google
-    else:
-        today_selected = today
-        df_selected = df_data_google
-#st.write('today {}'.format(today_selected))
+# SHOW THE TEXT, WHAT IS IT ETC.
+def show_infos():
+    st.title('__Format/Details:__')
 
-remaining_days = (date_race -today_selected).days
+    st.write('''
+    __Datum:__ 6.3.2021\n
+    __Start:__ 06.49\n
+    __Zielschluss:__ 18.13\n
+    __Technik:__ frei wählbar
 
-# define id and set as index
-df_selected['id'] = df_selected['Vorname']+' '+df_selected['Name']
-df_selected = df_selected.set_index('id',drop=False)
+    __Sieger*in__: Wer am __meisten Kilometer__ läuft in dieser Zeit.
+    Du bist eher der Typ "Bergfloh"? Dann laufe so viele __positiv Höhemeter__ wie du kannst und sichere dir den ersten Rang in der Kategorie "Bergpreis".\n
+    __Kategorien:__ Frauen, Männer
 
-#####################################################################################
-#####################################################################################
-#####################################################################################
-#st.title(':palm_tree: :hibiscus: From Dawn till Dusk :hibiscus: :palm_tree:')
-#st.write(df_data)
+    __Spezialwertung:__ Team Schweden vs. Team Doppelstock; weitere Battles sind gerne gesehen ;)
 
-#st.markdown('<img src="media/8b84de7d73fc3688748733868c78ff02c14995dabfe27ef33b090557.jpeg" alt="From Dawn Til Dusk" width="500" height="600">', unsafe_allow_html=True)
-#st.markdown('<img src="img_dawn_till_dusk.jpeg" alt="From Dawn Til Dusk" width="100%">', unsafe_allow_html=True)
-st.image('img_dawn_till_dusk.jpeg', width=site_width, use_column_width='always')
-if(remaining_days)>0:
-    #st.header('\n\n\n Nur noch {} Mal schlafen!\n\n'.format(remaining_days))
-    st.info('\n\n__Nur noch {} Mal schlafen!__\n\n'.format(remaining_days))
-    st.write('Noch nicht angemeldet? [Melde dich jetzt an!](http://bit.ly/3pg5KX3)')
+    __Messung:__ jede\*r Läufer\*in misst die Kilometer selbst mit einem Gerät/App nach Wunsch.
 
-    st.subheader('Angemeldet sind momentan {} Langläufer*innen'.format(df_selected['id'].count()))
-    for id in df_selected['id']:
-        st.write(id)
+    __Reporting:__ Kilometer, Höhenmeter und Lauftechnik per Text/Whatsapp/etc an Michael +41 (0)79 961 73 12 bis um 20.00 Uhr am Tag des Rennens (6.3.2021) senden. 
 
-else:
+    Bei Anregung oder Fragen, meldet euch einfach im Chat. 
+
+    Wir freuen uns auf einen grossartigen Lauf!
+
+    ''')
+
+# SHOW COMPLETE RESULTS WITH PLOTS
+def show_plots(df_selected):
     # RADIO
+    st.title('__Resultate__')
     sex = st.radio('',('alle','Frau','Herr'))
     if(sex=='alle'):
         df_selected = df_selected
@@ -126,7 +102,7 @@ else:
 
     ###########################
     # DISTANCE
-    st.header(':straight_ruler: Distanz')
+    st.header(':straight_ruler: __Distanz__')
     chart_distance = alt.Chart(
         df_selected#.sort_values(by='Km',ascending=False)
     ).mark_bar().encode(
@@ -139,7 +115,7 @@ else:
 
     ###########################
     # ALTITUDE
-    st.header(':snow_capped_mountain: Höhenmeter')
+    st.header(':snow_capped_mountain: __Höhenmeter__')
     chart_altitude = alt.Chart(
         df_selected
     ).mark_bar().encode(
@@ -152,7 +128,7 @@ else:
 
     ###########################
     # COMBINED
-    st.header(':muscle: Kombiniert')
+    st.header(':muscle: __Kombiniert__')
     chart_combined = alt.Chart(
         df_selected
     ).mark_circle().encode(
@@ -164,24 +140,72 @@ else:
     )
     st.altair_chart(chart_combined, use_container_width=True)
 
+#################
+# CHOOSE DATE
+today = datetime.date.today()
+# for testing:
+#today = datetime.date(2021,3,7)
+#st.write(today)
+date_race = datetime.date(2021,3,6)
+remaining_days = (date_race -today).days
+df_data_google = read_data()
+df_selected = df_data_google
+# if remaining_days>0:
+#     date = st.sidebar.radio('Navigation',('heute','postrace'),0)
+#     if(date=='postrace'):
+#         today_selected = datetime.date(2021, 3, 7)
+#         #df_selected = fill_with_dummy_data(df_data_google)#df_data_dummy
+#         df_selected = df_data_google
+#         #st.header('Die Plots zeigen Dummydaten')
+#     else:
+#         today_selected = datetime.date.today()
+#         df_selected = df_data_google
+# else:
+#     date = st.sidebar.radio('Navigation', ('prerace','heute'), 1)
+#     if (date == 'prerace'):
+#         today_selected = datetime.date(2021, 3, 5)
+#         df_selected = df_data_google
+#     else:
+#         today_selected = today
+#         df_selected = df_data_google
+#st.write('today {}'.format(today_selected))
+#remaining_days = (date_race -today_selected).days
+
+def img_to_bytes(img_path):
+    img_bytes = Path(img_path).read_bytes()
+    encoded = base64.b64encode(img_bytes).decode()
+    return encoded
+
+# define id and set as index
+df_selected['Technik'] = df_selected['Technik'].fillna(value='Skating')
+df_selected['id'] = df_selected['Vorname']+' '+df_selected['Name']
+df_selected = df_selected.set_index('id',drop=False)
+
+#####################################################################################
+#####################################################################################
+#####################################################################################
+
+# IMAGE
+st.image('banner_dawn_till_dusk.jpeg', width=site_width, use_column_width='always')
+
+# SHOW GENERAL RULES AND INFOS
+show_infos()
+
+if(remaining_days)>0:
+    #st.header('\n\n\n Nur noch {} Mal schlafen!\n\n'.format(remaining_days))
+    st.info('\n\n__Nur noch {} Mal schlafen!__\n\n'.format(remaining_days))
+    st.write('Noch nicht angemeldet? [Melde dich jetzt an!](http://bit.ly/3pg5KX3)')
+
+    st.subheader('Angemeldet sind momentan {} Langläufer*innen'.format(df_selected['id'].count()))
+    for id in df_selected['id']:
+        st.write(id)
+
+# SHOW THE RESULTS AND PLOTS
+show_plots(df_selected)
 ##############################################################################
 # FOOTER
 
 st.info(""":palm_tree: :hibiscus: :palm_tree: :hibiscus: :palm_tree: :hibiscus: :palm_tree: :hibiscus: :palm_tree: :hibiscus: :palm_tree:
 
-[Langlaufklub Züri Doppelstock](https://www.zuerich-doppelstock.com/)
-
 Mehr Infos gibt es in unserer WhatsApp-Gruppe.
 """)
-
-
-###################
-# Friedhof
-# SELECTION OF NAMES
-#skiers = st.multiselect(
-#        "Wähle Leute", list(df_data['id']),list(df_data['id'])#, ["China", "United States of America"]
-#)
-#if not skiers:
-#    st.error("Please select at least one country.")
-#else:
-#df_selected = df_selected.loc[skiers]
